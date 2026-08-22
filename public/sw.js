aa147f5cd8a12a0e305de99781dc06932f8766f1
jurgenvang <jurgenvang@gmail.com>
2026-08-22 09:48:48 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Kleine service worker: de app blijft bruikbaar zonder bereik,
 // maar gegevens komen altijd vers van de server als dat kan.
-const CACHE = 'whats4dinner-v2';
+const CACHE = 'whats4dinner-v3';
 const SCHIL = ['/', '/index.html', '/manifest.webmanifest', '/icoon.svg', '/icoon-180.png', '/icoon-192.png'];
 
 self.addEventListener('install', e => {
